d021b8a64f71211d6644a6e5d557c0dca2bc77bd
Son7 <sonphan.xcode@gmail.com>
2026-05-14 15:00:38 +0700

    create ProjectDescription

--- Project.swift
+++ Project.swift
@@ -1,37 +1,57 @@
 import ProjectDescription
 
-let project = Project(
-    name: "Test3",
-    targets: [
-        .target(
-            name: "Test3",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "dev.tuist.Test3",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
-            buildableFolders: [
-                "Test3/Sources",
-                "Test3/Resources",
-            ],
-            dependencies: []
-        ),
-        .target(
-            name: "Test3Tests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "dev.tuist.Test3Tests",
-            infoPlist: .default,
-            buildableFolders: [
-                "Test3/Tests"
-            ],
-            dependencies: [.target(name: "Test3")]
-        ),
-    ]
-)
+private let bundleId: String = "dev.son7.ios.ptest3"
+private let version: String = "0.0.1"
+private let bundleVersion: String = "1"
+private let iOSTargetVersion: String = "16.0"
+
+private let basePath: String = "Targets/PTest3"
+private let appName = "PTest3"
+
+let project = Project(name: appName,
+                      packages: [],
+                      settings: Settings.settings(configurations: makeConfigurations()),
+                      targets: [
+                        .target(name: "iOS",
+                                destinations: [.iPhone],
+                                product: .app, bundleId: bundleId,
+                                deploymentTargets: .iOS(iOSTargetVersion),
+                                infoPlist: makeInfoPlist(),
+                                sources: ["\(basePath)/Sources/**"],
+                                resources: ["\(basePath)/Resources/**"],
+                                settings: baseSettings()
+                               )
+                      ],
+                      additionalFiles: [
+                        "README.md",
+                      ])
+
+
+private func makeConfigurations() -> [Configuration] {
+    let debug: Configuration = Configuration.debug(name: "Debug", xcconfig: "Configs/Debug.xcconfig")
+    let release: Configuration = Configuration.release(name: "Release", xcconfig: "Configs/Release.xcconfig")
+    
+    return [debug, release]
+}
+
+private func makeInfoPlist(merging other: [String : Plist.Value] = [:]) -> InfoPlist {
+    var extendedPlist: [String : Plist.Value] = [:]
+    extendedPlist = ["UIApplicationSceneManifest": .dictionary(["UIApplicationSupportsMultipleScenes": .boolean(true)]),
+                     "UILaunchScreen": .array([]),
+                     "UISupportedInterfaceOrientations": .array([.string("UIInterfaceOrientationPortrait")]),
+                     "CFBundleShortVersionString": .string(version),
+                     "CFBundleVersion": .string(bundleVersion),
+                     "CFBundleDisplayName": .string(appName)
+                    ]
+    other.forEach { (key: String, value: Plist.Value) in
+        extendedPlist[key] = value
+    }
+    return .extendingDefault(with: extendedPlist)
+}
+
+private func baseSettings() -> Settings {
+    let settings = SettingsDictionary()
+    return Settings.settings(base: settings,
+                             configurations: [],
+                             defaultSettings: .recommended)
+}
